8defd414a8fa657e66c105c6b43bb30c9157e040
Endybits <endyb.dev@gmail.com>
2024-02-25 22:59:40 +1100

    manual CRUD

--- main.py
+++ main.py
@@ -5,11 +5,30 @@ add_user = UserRepo().add
 
 user_example = {
     "name": "Endy",
-    "email": "eb_azul@email.co"
+    "email": "eb_updated_azul@email.co"
 }
 
+def get_all_users():
+    return UserRepo().fetch_all()
+
 def main():
-    user = add_user(user=user_example)
+    # Add user
+    # user = add_user(user=user_example)
+    
+    # Get all users
+    users = get_all_users()
+    for user in users:
+        print(user.id, user.name, user.email)
+    
+    # Update user
+    user = UserRepo().update(2, user_example)
+
+    # Get user by id
+    user = UserRepo().fetch_by_id(2)
+    print(user.id, user.name, user.email)
+    
+    # Delete user
+    user = UserRepo().delete(2)
     print(user)
 
 main()
--- user_crud.py
+++ user_crud.py
@@ -21,3 +21,23 @@ class UserRepo:
         self.db.commit()
         self.db.refresh(user_obj)
         return f"Added user {user_obj.name} with email {user_obj.email}"
+
+    def fetch_by_id(self, user_id: int):
+        return self.db.query(User).filter(User.id == user_id).first()
+
+    def fetch_all(self, skip: int = 0, limit: int = 10):
+        return self.db.query(User).offset(skip).limit(limit).all()
+
+    def delete(self, user_id: int):
+        user = self.fetch_by_id(user_id)
+        self.db.delete(user)
+        self.db.commit()
+        return f"Deleted user {user.name} with email {user.email}"
+
+    def update(self, user_id: int, user: dict):
+        user_obj = self.fetch_by_id(user_id)
+        user_obj.name = user['name'] if user['name'] else user.name
+        user_obj.email = user['email'] if user['email'] else user.email
+        self.db.commit()
+        self.db.refresh(user_obj)
+        return f"Updated user {user_obj.name} with email {user_obj.email}"
